2848bd53aab6ed6597cb1f5f8f6a09c2da70f21c
J-Older <zeesnippi@gmail.com>
2020-02-25 14:54:03 +0200

    RECO ver. 1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $('html, body, *').mousewheel(function (e, delta) {
     // multiplying by 40 is the sensitivity,
     // increase to scroll faster.
-    this.scrollLeft -= (delta * 1);
+    this.scrollLeft -= (delta * 40);
     e.preventDefault();
 });
 
-
-    $(window).on("scroll resize", function() {
-        var o = $(window).scrollLeft() / ($(document).width() - $(window).width());
-        $(".progress-bar").css({
-            "width": (100 * o | 0) + "%"
-        });
-        $('.progress')[0].value = o;
-    });
-
-
+//
+//     $(window).on("scroll resize", function() {
+//         var o = $(window).scrollLeft() / ($(document).width() - $(window).width());
+//         $(".progress-bar").css({
+//             "width": (100 * o | 0) + "%"
+//         });
+//         $('.progress')[0].value = o;
+//     });
+//
+//
