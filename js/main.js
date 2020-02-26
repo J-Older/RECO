11aa19f11c5bf5a6be4959616b6839c5d508533f
J-Older <zeesnippi@gmail.com>
2020-02-26 15:39:45 +0200

    RECO ver. 1

--- js/main.js
+++ js/main.js
@@ -5,6 +5,54 @@ $('html, body, *').mousewheel(function (e, deltaX) {
     // e.preventDefault();
 });
 
+var tabs = $(".menu-tabs");
+var doc = $("#main");
+
+
+$(tabs).on("click", ".tab-first", function () {
+    $("body .section").removeClass("active");
+    $("#first").addClass("active");
+    $('body,html').animate({
+        scrollTop: 0
+    }, 400);
+    return false;
+});
+
+$(tabs).on("click", ".tab-second", function () {
+    $("body .section").removeClass("active");
+    $("#second").addClass("active");
+    $('body,html').animate({
+        scrollTop: 0
+    }, 400);
+    return false;
+});
+
+$(tabs).on("click", ".tab-third", function () {
+    $("body .section").removeClass("active");
+    $("#third").addClass("active");
+    $('body,html').animate({
+        scrollTop: 0
+    }, 400);
+    return false;
+});
+
+$(tabs).on("click", ".tab-fourth", function () {
+    $("body .section").removeClass("active");
+    $("#fourth").addClass("active");
+    $('body,html').animate({
+        scrollTop: 0
+    }, 400);
+    return false;
+});
+
+$(tabs).on("click", ".tab-fifth", function () {
+    $("body .section").removeClass("active");
+    $("#fifth").addClass("active");
+    $('body,html').animate({
+        scrollTop: 0
+    }, 400);
+    return false;
+});
 
 
 //     $(window).on("scroll resize", function() {
