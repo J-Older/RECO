56038613a7e4af9a066fe4b3edfc4e5201f36f6b
J-Older <48174604+J-Older@users.noreply.github.com>
2020-03-10 21:45:47 +0200

    RECO ver. 3

--- js/main.js
+++ js/main.js
@@ -73,3 +73,9 @@ $(function () {
 });
 
 
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(500).fadeOut('slow');
+});
